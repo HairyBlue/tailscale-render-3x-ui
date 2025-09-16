9a174a17a9916a5eea6edd067ab9a7df88a5ad74
Hairy Blue <pecision.nickimarty@gmail.com>
2025-09-16 08:07:41 +0800

    init kape

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
+/app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=render-app  --advertise-exit-node
+
+# Wait a few seconds for Tailscale to fully initialize
+sleep 10
+
+# Start 3X-UI in background (adjust path if needed)
+/usr/local/bin/3x-ui &
+
+/app/my-app
