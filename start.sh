0ffe156df92a32a31621366e1445dfbe67972bad
Hairy Blue <pecision.nickimarty@gmail.com>
2025-09-16 08:13:28 +0800

    ass bash

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 /app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=render-app  --advertise-exit-node
